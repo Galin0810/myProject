03cc3d6006834ef49a5e6c7cb0edc046414fe7de
Domain_G <galin08domain@gmail.com>
2017-10-02 17:39:40 +0300

    My First commit

---
+++ src/Dispatcher.java
@@ -0,0 +1,8 @@
+/**
+ * Created by domain on 02.10.17.
+ */
+public class Dispatcher {
+    public static void main(String[] args) {
+System.out.print(new Printer().showMessage("Hello,"));
+    }
+}
---
+++ src/Printer.java
@@ -0,0 +1,9 @@
+/**
+ * Created by domain on 02.10.17.
+ */
+public class Printer {
+    public String showMessage(String greeting){
+        greeting= greeting + " World!";
+        return greeting;
+    }
+}
